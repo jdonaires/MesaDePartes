f2689ffeb6579cf0136e751c7eeb1730e376b57e
fiorella perez gomez <fiorellapg1998@gmail.com>
2018-11-03 17:42:06 -0500

    Eliminado
    Se acaba de Eliminar una prueba que se hizo el 31 de Octubre

--- PROYECTO/Aaron_Pasache.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo '73816474';
-echo 'Ingeniero de sistemas';
- ?>
- <?php
- echo 'mi portada es de Condorito';
-  ?>
--- PROYECTO/ortiz.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '70401854';
-echo 'Ingeniero de sistemas';
- ?>
- <?php
-  ?>
