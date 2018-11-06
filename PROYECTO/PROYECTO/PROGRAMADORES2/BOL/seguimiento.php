2d74361359f6270e1837d4be9f994bd5a7ae2e11
karen <42850487+karenbriyith@users.noreply.github.com>
2018-11-06 15:58:52 -0500

    agregando
    las variables function que me falto modificar

--- PROYECTO/PROYECTO/PROGRAMADORES2/BOL/seguimiento.php
+++ PROYECTO/PROYECTO/PROGRAMADORES2/BOL/seguimiento.php
@@ -10,13 +10,13 @@ class seguimiento
   private $CodigoDoc;
   private $idPersonal;
 
-  public function __GET($x)
+  public function __GET($id)
 	{
-		return $this->$x;
+		return $this->$id;
 	}
-	public function __SET($x, $y)
+	public function __SET($id, $idPersonal)
 	{
-		return $this->$x = $y;
+		return $this->$id = $idPersonal;
 	}
 }
 ?>
