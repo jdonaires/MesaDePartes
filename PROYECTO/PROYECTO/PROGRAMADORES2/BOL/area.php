84865c1aa87ebccd93c7d58a3ad74dc96a57e70c
karen <42850487+karenbriyith@users.noreply.github.com>
2018-11-05 19:16:48 -0500

    agregando las clases a la capa BOL
    modificando las clases faltantes de mesa de partes

---
+++ PROYECTO/PROYECTO/PROGRAMADORES2/BOL/area.php
@@ -0,0 +1,17 @@
+<?php
+class area
+{
+	private $id;
+	private $Area;
+
+
+  public function __GET($x)
+	{
+		return $this->$x;
+	}
+	public function __SET($x, $y)
+	{
+		return $this->$x = $y;
+	}
+}
+?>
