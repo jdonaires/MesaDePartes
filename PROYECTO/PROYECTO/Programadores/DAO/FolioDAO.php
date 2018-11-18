5f6e4f87d3c401a8041d1294af6247cf80dfb348
Aaron Daniel Pasache Pachas <42815436+Aaron20183@users.noreply.github.com>
2018-11-18 15:50:47 -0500

    funcion folioDAO
    se creó la función DAO para la tabla folio, primero se tuvo que añadir esta parte del codigo total, que ivolucra el acceso al dbaccess y a la funcion folio gracias a la funcion CONSTRUCT, luego sigue la funcion REGISTRAR para los atributos de la clase

--- PROYECTO/PROYECTO/Programadores/DAO/FolioDAO.php
+++ PROYECTO/PROYECTO/Programadores/DAO/FolioDAO.php
@@ -1,5 +1,39 @@
 /* Aaaron */
+<?php
+require_once('../DAL/DBAccess.php');
+require_once('../BOL/Folio.php');
 
+class folioDAO
+{
+	private $pdo;
+
+	public function __CONSTRUCT()
+	{
+			$dba = new DBAccess();
+			$this->pdo = $dba->get_connection();
+	}
+
+	public function Registrar(Folio $Folio)
+	{
+		try
+		{
+		$statement = $this->pdo->prepare("insert into folio values(?,?,?)");
+		$statement->bindParam(1,$Folio->__GET('Id'));
+    	$statement->bindParam(2,$Folio->__GET('CantidadDocumento'));
+		$statement->bindParam(3,$Folio->__GET('NroFolio'));
+    	$statement -> execute();
+
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
+
+	public function Listar(Folio $Folio)
+	{
+		try
+		{
+			$result = array();
 
 
 
