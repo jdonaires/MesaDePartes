6585200b09b1afa7574ce85be18ba59864b6252a
Daniel12jk1 <44442287+Daniel12jk1@users.noreply.github.com>
2018-11-19 13:34:01 -0500

    condicion
    elije,se ejecuta y luego se pone una condicion

--- PROYECTO/PROYECTO/Programadores/DAO/AreaDAO.php
+++ PROYECTO/PROYECTO/Programadores/DAO/AreaDAO.php
@@ -36,7 +36,15 @@ class AreaDAO
 $statement = $this->pdo->prepare("call up_buscar_Area(?)");
 $statement->bindParam(1,$Area->__GET('Area'));
 $statement->execute();
+foreach($statement->fetchAll(PDO::FETCH_OBJ) as $r)
+			{
+				$ar = new Area();
 
+				$ar->__SET('Id', $r->idarea);
+				$ar->__SET('Area', $r->Area);
+
+				$result[] = $ar;
+			}
 
 
 
