744197ef865236545d6689d698cfc8287664352e
josue <josue.sa.1995@gmail.com>
2018-11-19 15:25:36 -0500

    Se crea la clase EmpresaDAO:
    Se instancia el pdo por el cual se hace a conexión a la base de datos.

--- PROYECTO/PROYECTO/Programadores/DAO/EmpresaDAO.php
+++ PROYECTO/PROYECTO/Programadores/DAO/EmpresaDAO.php
@@ -2,5 +2,15 @@
 require_once('../DAL/DBAccess.php');
 require_once('../BOL/Empresa.php');
 
+class EmpresaDAO
+{
+	private $pdo;
+
+	public function __CONSTRUCT()
+	{
+			$dba = new DBAccess();
+			$this->pdo = $dba->get_connection();
+	}
+}
 
 ?>
