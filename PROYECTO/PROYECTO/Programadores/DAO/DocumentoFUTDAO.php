a744e47f1d767e2894afb2aca72d98ffa930f3be
RafaelGutierrez <rafaelmartingr@gmail.com>
2018-11-20 12:45:55 -0500

    funciones
    se crea la funcion registra del documento fut
    con un trycatch

--- PROYECTO/PROYECTO/Programadores/DAO/DocumentoFUTDAO.php
+++ PROYECTO/PROYECTO/Programadores/DAO/DocumentoFUTDAO.php
@@ -12,6 +12,21 @@ class DocumentoFutDAO
 			$this->pdo = $dba->get_connection();
 	}
 
+  public function Registrar(DocumentoFut $DocumentoFut)
+	{
+		try
+		{
+		$statement = $this->pdo->prepare("insert into documentofut values(?,?,?,?)");
+		$statement->bindParam(1,$DocumentoFut->__GET('CodigoDoc'));
+    	$statement->bindParam(2,$DocumentoFut->__GET('CasualResignacion'));
+		$statement->bindParam(3,$DocumentoFut->__GET('IdTipoPlazas'));
+   	 	$statement->bindParam(4,$DocumentoFut->__GET('IdPersonal'));
+    	$statement -> execute();
 
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
 }
 ?>
