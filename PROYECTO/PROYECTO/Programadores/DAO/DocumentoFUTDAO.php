83cf131e195aef98a35d61ac633b489371f3c9cf
RafaelGutierrez <rafaelmartingr@gmail.com>
2018-11-20 12:43:46 -0500

    Gutierrez
    Creacion de la clase documentoFUT con su respectivo constructor

--- PROYECTO/PROYECTO/Programadores/DAO/DocumentoFUTDAO.php
+++ PROYECTO/PROYECTO/Programadores/DAO/DocumentoFUTDAO.php
@@ -1,4 +1,17 @@
 <?php
 require_once('../DAL/DBAccess.php');
 require_once('../BOL/DocumentoFut.php');
+
+class DocumentoFutDAO
+{
+	private $pdo;
+
+	public function __CONSTRUCT()
+	{
+			$dba = new DBAccess();
+			$this->pdo = $dba->get_connection();
+	}
+
+
+}
 ?>
