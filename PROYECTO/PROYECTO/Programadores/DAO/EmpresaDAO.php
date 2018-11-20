016f014be2dc2931771f476a5b28555ec099f5d1
josue <josue.sa.1995@gmail.com>
2018-11-19 21:55:00 -0500

    Creación de la función registrar para  la clase empresaDAO:
    En este se prepara a insertar  los campos de la empresa.

--- PROYECTO/PROYECTO/Programadores/DAO/EmpresaDAO.php
+++ PROYECTO/PROYECTO/Programadores/DAO/EmpresaDAO.php
@@ -11,6 +11,21 @@ class EmpresaDAO
 			$dba = new DBAccess();
 			$this->pdo = $dba->get_connection();
 	}
+	public function Registrar(Empresa $Empresa)
+	{
+		try
+		{
+		$statement = $this->pdo->prepare ("insert into empresa values(?,?,?)");
+		$statement->bindvalue(1,$Empresa->__GET('Id'));
+    	$statement->bindvalue(2,$Empresa->__GET('RazonSocial'));
+		$statement->bindvalue(3,$Empresa->__GET('RUC'));
+    	$statement -> execute();
+
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
 }
 
 ?>
