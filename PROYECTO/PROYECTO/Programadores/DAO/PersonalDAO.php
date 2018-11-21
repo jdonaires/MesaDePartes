a88b1c2dcc76c25849bfc8587d448ff5c28e144d
FiorelaVeraC <42990408+FiorelaVeraC@users.noreply.github.com>
2018-11-20 20:34:41 -0500

    Funcion Registrar PersonalDAO
    funcion publica que se encargara de Registrar el objeto de clase Personal, la funcion se encuentra en un try de esta clase utilizamos el campo pdo y preparamos la consulta donde insertaremos los valores - una ves obtenidos los valores se ejecuta la consulta - catch ve si ocurre algun error mosrtrara un mesje

--- PROYECTO/PROYECTO/Programadores/DAO/PersonalDAO.php
+++ PROYECTO/PROYECTO/Programadores/DAO/PersonalDAO.php
@@ -11,3 +11,21 @@ class personalDAO
 			$dba = new DBAccess();
 			$this->pdo = $dba->get_connection();
 	}
+	public function Registrar(Personal $Personal)
+		{
+			try
+			{
+			$statement = $this->pdo->prepare("insert into personal values(?,?,?,?,?,?)");
+			$statement->bindParam(1,$Personal->__GET('Id'));
+	    $statement->bindParam(2,$Personal->__GET('Cargo'));
+			$statement->bindParam(3,$Personal->__GET('IdPersona'));
+			$statement->bindParam(4,$Personal->__GET('Usuario'));
+			$statement->bindParam(5,$Personal->__GET('Contraseña'));
+			$statement->bindParam(6,$Personal->__GET('Estado'));
+	    $statement -> execute();
+
+			} catch (Exception $e)
+			{
+				die($e->getMessage());
+			}
+		}
