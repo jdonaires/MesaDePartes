07094a6dfde0cc1f2f6c5c018da4ef338909a031
anthoni95 <43188012+anthoni95@users.noreply.github.com>
2018-11-21 06:09:08 -0500

    Funcion Registrar RegistroExpedienteDAO
    funcion publica que se encargara de Registrar el objeto de clase RegistroExpediente, la funcion se encuentra en un try de esta clase utilizamos el campo pdo y preparamos la consulta donde insertaremos los valores - una ves obtenidos los valores se ejecuta la consulta - catch ve si ocurre algun error mosrtrara un mensaje

--- PROYECTO/PROYECTO/Programadores/DAO/RegistroExpedienteDAO.php
+++ PROYECTO/PROYECTO/Programadores/DAO/RegistroExpedienteDAO.php
@@ -12,6 +12,23 @@ class RegistroExpedienteDAO
 			$dba = new DBAccess();
 			$this->pdo = $dba->get_connection();
 	}
+  public function Registrar(RegistroExpediente $RegistroExpediente)
+	{
+		try
+		{
+		$statement = $this->pdo->prepare("insert into registroexpediente values(?,?,?,?,?)");
+		$statement->bindParam(1,$RegistroExpediente->__GET('Id'));
+    	$statement->bindParam(2,$RegistroExpediente->__GET('NExpediente'));
+		$statement->bindParam(3,$RegistroExpediente->__GET('FechaIngreso'));
+   	 	$statement->bindParam(4,$RegistroExpediente->__GET('idFolio'));
+    	$statement->bindParam(5,$RegistroExpediente->__GET('CodigoDoc'));
+    	$statement -> execute();
+
+		} catch (Exception $e)
+		{
+			die($e->getMessage());
+		}
+	}
 }
 
 ?>
