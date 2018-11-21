7a492e5e843f405bfe62efec1eba73e83fc443ae
anthoni95 <43188012+anthoni95@users.noreply.github.com>
2018-11-21 06:19:07 -0500

    Funcion listar RegistroExpediente
    funcion publica que listar el objeto de clase registroexpediente, la funcion se encuentra en un try de esta clase utilizamos el campo pdo preparamos la consulta donde llamamos a parametro,ejecutamos la consulta,con el fetchAll obtenemos todos los resultados que devuelve la bd, creamos la variable regexp que se convierte en un objeto de registroexpediente, al objeto regexp le establecemos los valores y luego el objeto regexp se introduce a la variable result y es de tipo array

--- PROYECTO/PROYECTO/Programadores/DAO/RegistroExpedienteDAO.php
+++ PROYECTO/PROYECTO/Programadores/DAO/RegistroExpedienteDAO.php
@@ -17,9 +17,9 @@ class RegistroExpedienteDAO
 		try
 		{
 		$statement = $this->pdo->prepare("insert into registroexpediente values(?,?,?,?,?)");
-		$statement->bindParam(1,$RegistroExpediente->__GET('Id'));
+		  $statement->bindParam(1,$RegistroExpediente->__GET('Id'));
     	$statement->bindParam(2,$RegistroExpediente->__GET('NExpediente'));
-		$statement->bindParam(3,$RegistroExpediente->__GET('FechaIngreso'));
+		  $statement->bindParam(3,$RegistroExpediente->__GET('FechaIngreso'));
    	 	$statement->bindParam(4,$RegistroExpediente->__GET('idFolio'));
     	$statement->bindParam(5,$RegistroExpediente->__GET('CodigoDoc'));
     	$statement -> execute();
@@ -29,6 +29,36 @@ class RegistroExpedienteDAO
 			die($e->getMessage());
 		}
 	}
+  public function Listar(RegistroExpediente $RegistroExpediente)
+  {
+  try
+  {
+    $result = array();
+
+    $statement = $this->pdo->prepare("call up_buscar_registroexpediente(?)");
+    $statement->bindParam(1,$registroexpediente->__GET('NroExpediente'));
+    $statement->execute();
+
+    foreach($statement->fetchAll(PDO::FETCH_OBJ) as $r)
+    {
+      $regexp = new Registroexp();
+
+      $regexp->__SET('Id', $r->idregistroexp);
+      $regexp->__SET('NExpediente', $r->NroExp);
+      $regexp->__SET('FechaIngreso', $r->FechaIngreso);
+          $regexp->__SET('idFolio', $r->IdFolio);
+          $regexp->__SET('CodigoDoc', $r->CodigoDoc);
+
+      $result[] = $regexp;
+    }
+
+    return $result;
+  }
+  catch(Exception $e)
+  {
+    die($e->getMessage());
+  }
+ }
 }
 
 ?>
